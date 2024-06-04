cf368602137a6f8302c5bc8f456a673e6aa6d8b2
tom <tomd1415@gmail.com>
2024-06-04 00:03:53 +0000

    removing unneeded files

--- add_subject_year_promps.sql
+++
@@ -1,9 +0,0 @@
-USE comment_bank;
-CREATE TABLE SubjectYearGroupPrompts (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    subjectId INT,
-    yearGroupId INT,
-    promptPart TEXT,
-    FOREIGN KEY (subjectId) REFERENCES Subjects(id),
-    FOREIGN KEY (yearGroupId) REFERENCES YearGroups(id)
-);
