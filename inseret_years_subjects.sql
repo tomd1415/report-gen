28305468f30ea35fc55731ceef876156e0c9b584
tom <tomd1415@gmail.com>
2024-06-04 00:07:27 +0000

    removing unneeded files

--- inseret_years_subjects.sql
+++
@@ -1,7 +0,0 @@
-USE comment_bank;
-
--- Insert initial data into Subjects
-INSERT INTO Subjects (name) VALUES ('Art'),('Computing'),('English'),('Maths');
-
--- Insert initial data into YearGroups
-INSERT INTO YearGroups (name) VALUES ('Year 7'),('Year 8'),('Year 9'),('Year 10'),('Year 11'),('Year 12'),('Year 13');
